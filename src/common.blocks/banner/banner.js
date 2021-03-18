104bd9fa0582a42a75d646eb1fa1c1136de34fd3
Andrew <uytov1991@gmail.com>
2021-03-18 15:51:58 +0300

    адаптировать баннер под десктоп

--- src/common.blocks/banner/banner.js
+++ src/common.blocks/banner/banner.js
@@ -7,21 +7,28 @@ gsap.registerPlugin(Draggable)
 
 const bannerContainer = document.querySelector('.page-main__banner')
 const bannerList = bannerContainer.querySelector('.banner__carousel-list')
+const bannerControllsCircles = bannerContainer.querySelectorAll(
+  '.banner__controll-item'
+)
 
 Draggable.create(bannerList, {
   type: 'x',
   bounds: bannerContainer,
   edgeResistance: 0.65,
+  zIndexBoost: false,
   onDragEnd: function() {
     let widthWindow = document.documentElement.clientWidth
     let bannerItemWidth = 94
     let marginRightBannerItem = 10
+
     let step = (widthWindow * bannerItemWidth) / 100 + marginRightBannerItem
+
     let isToggle =
       Math.abs((Math.abs(this.startX) - Math.abs(this.x)) / step) >= 0.2
         ? true
         : false
 
+    let currentSlide = Math.abs(this.startX) / step
     let direction = this.getDirection('start')
 
     if (direction === 'right' && isToggle && this.startX !== 0) {
@@ -29,7 +36,17 @@ Draggable.create(bannerList, {
         x: this.startX + step,
         duration: 0.3,
       })
-      return
+
+      bannerControllsCircles[currentSlide].classList.remove(
+        'banner__controll-item--active'
+      )
+      bannerControllsCircles[currentSlide - 1].classList.add(
+        'banner__controll-item--active'
+      )
+      gsap.from(bannerControllsCircles[currentSlide - 1], {
+        scale: 1.4,
+        duration: 0.3,
+      })
     } else if (
       direction === 'left' &&
       isToggle &&
@@ -39,7 +56,17 @@ Draggable.create(bannerList, {
         x: this.startX - step,
         duration: 0.3,
       })
-      return
+
+      bannerControllsCircles[currentSlide].classList.remove(
+        'banner__controll-item--active'
+      )
+      bannerControllsCircles[currentSlide + 1].classList.add(
+        'banner__controll-item--active'
+      )
+      gsap.from(bannerControllsCircles[currentSlide + 1], {
+        scale: 1.4,
+        duration: 0.3,
+      })
     } else {
       gsap.to(this.target, {
         x: this.startX,
