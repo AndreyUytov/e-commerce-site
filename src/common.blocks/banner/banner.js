091d545a888b32949ce298a9649550b4b8178e53
Andrew <uytov1991@gmail.com>
2021-03-18 14:33:52 +0300

    мобильный баннер готов

--- src/common.blocks/banner/banner.js
+++ src/common.blocks/banner/banner.js
@@ -5,14 +5,46 @@ import { Draggable } from 'gsap/Draggable'
 
 gsap.registerPlugin(Draggable)
 
-const eventsUnify = function(e) {
-  return e.changedTouches ? e.changedTouches[0] : e
-}
-
 const bannerContainer = document.querySelector('.page-main__banner')
 const bannerList = bannerContainer.querySelector('.banner__carousel-list')
 
 Draggable.create(bannerList, {
   type: 'x',
   bounds: bannerContainer,
+  edgeResistance: 0.65,
+  onDragEnd: function() {
+    let widthWindow = document.documentElement.clientWidth
+    let bannerItemWidth = 94
+    let marginRightBannerItem = 10
+    let step = (widthWindow * bannerItemWidth) / 100 + marginRightBannerItem
+    let isToggle =
+      Math.abs((Math.abs(this.startX) - Math.abs(this.x)) / step) >= 0.2
+        ? true
+        : false
+
+    let direction = this.getDirection('start')
+
+    if (direction === 'right' && isToggle && this.startX !== 0) {
+      gsap.to(this.target, {
+        x: this.startX + step,
+        duration: 0.3,
+      })
+      return
+    } else if (
+      direction === 'left' &&
+      isToggle &&
+      this.startX !== this.target.offsetWidth - step
+    ) {
+      gsap.to(this.target, {
+        x: this.startX - step,
+        duration: 0.3,
+      })
+      return
+    } else {
+      gsap.to(this.target, {
+        x: this.startX,
+        duration: 0.3,
+      })
+    }
+  },
 })
