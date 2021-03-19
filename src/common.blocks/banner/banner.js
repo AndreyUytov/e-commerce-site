fc325d3ba7db05e1eed2ec4c30179519508bab43
Andrew <uytov1991@gmail.com>
2021-03-19 15:59:15 +0300

    проверить слайдер(возможно переписать под класс)

--- src/common.blocks/banner/banner.js
+++ src/common.blocks/banner/banner.js
@@ -7,9 +7,94 @@ gsap.registerPlugin(Draggable)
 
 const bannerContainer = document.querySelector('.page-main__banner')
 const bannerList = bannerContainer.querySelector('.banner__carousel-list')
+const bannerListItem = bannerList.querySelector('.banner__carousel-item')
+const slidersLength = bannerList.querySelectorAll('.banner__carousel-item')
+  .length
 const bannerControllsCircles = bannerContainer.querySelectorAll(
   '.banner__controll-item'
 )
+const bannerSnapLeft = document.querySelector(
+  '.banner-wrapper__controll-snap--left'
+)
+const bannerSnapRight = document.querySelector(
+  '.banner-wrapper__controll-snap--right'
+)
+
+let currentSlide = 0 //1st Slide = 0, 2st Slide = 1
+
+const stepInPixels = (bannerListItem) => {
+  let bannerItemWidth = bannerListItem.offsetWidth
+  let marginRightBannerItem = parseInt(
+    getComputedStyle(bannerListItem).marginRight
+  )
+  let step = bannerItemWidth + marginRightBannerItem
+
+  return step
+}
+
+const currentSliderUpdater = (nextSlideIndex) => {
+  if (nextSlideIndex > 0 && nextSlideIndex < slidersLength - 1) {
+    bannerSnapLeft.disabled = false
+    bannerSnapRight.disabled = false
+  }
+  if (nextSlideIndex === 0) {
+    bannerSnapLeft.disabled = true
+    bannerSnapRight.disabled = false
+  }
+
+  if (nextSlideIndex === slidersLength - 1) {
+    bannerSnapRight.disabled = true
+    bannerSnapLeft.disabled = false
+  }
+
+  bannerControllsCircles[currentSlide].classList.remove(
+    'banner__controll-item--active'
+  )
+  bannerControllsCircles[nextSlideIndex].classList.add(
+    'banner__controll-item--active'
+  )
+  currentSlide = nextSlideIndex
+
+  gsap.from(bannerControllsCircles[currentSlide], {
+    scale: 1.4,
+    duration: 0.3,
+  })
+}
+
+bannerSnapRight.addEventListener('click', () => {
+  if (currentSlide !== slidersLength - 1) {
+    let step = stepInPixels(bannerListItem)
+    let currentSlidePosistion = gsap.getProperty(bannerList, 'x')
+    gsap.to(bannerList, {
+      x: currentSlidePosistion - step,
+      duration: 0.3,
+    })
+    currentSliderUpdater(currentSlide + 1)
+  }
+})
+
+bannerSnapLeft.addEventListener('click', () => {
+  if (currentSlide !== 0) {
+    let step = stepInPixels(bannerListItem)
+    let currentSlidePosistion = gsap.getProperty(bannerList, 'x')
+    gsap.to(bannerList, {
+      x: currentSlidePosistion + step,
+      duration: 0.3,
+    })
+    currentSliderUpdater(currentSlide - 1)
+  }
+})
+
+window.addEventListener('resize', () => {
+  if (currentSlide) {
+    gsap.to(bannerList, {
+      x: 0,
+      duration: 0.3,
+    })
+
+    currentSliderUpdater(0)
+  }
+})
 
 Draggable.create(bannerList, {
   type: 'x',
@@ -17,54 +102,29 @@ Draggable.create(bannerList, {
   edgeResistance: 0.65,
   zIndexBoost: false,
   onDragEnd: function() {
-    let widthWindow = document.documentElement.clientWidth
-    let bannerItemWidth = 94
-    let marginRightBannerItem = 10
-
-    let step = (widthWindow * bannerItemWidth) / 100 + marginRightBannerItem
-
+    let step = stepInPixels(bannerListItem)
     let isToggle =
       Math.abs((Math.abs(this.startX) - Math.abs(this.x)) / step) >= 0.2
         ? true
         : false
 
-    let currentSlide = Math.abs(this.startX) / step
+    // currentSlide = Math.floor(Math.abs(this.startX) / step)
     let direction = this.getDirection('start')
 
-    if (direction === 'right' && isToggle && this.startX !== 0) {
+    if (direction === 'right' && isToggle && currentSlide !== 0) {
+      currentSliderUpdater(currentSlide - 1)
       gsap.to(this.target, {
-        x: this.startX + step,
-        duration: 0.3,
-      })
-
-      bannerControllsCircles[currentSlide].classList.remove(
-        'banner__controll-item--active'
-      )
-      bannerControllsCircles[currentSlide - 1].classList.add(
-        'banner__controll-item--active'
-      )
-      gsap.from(bannerControllsCircles[currentSlide - 1], {
-        scale: 1.4,
+        x: -currentSlide * step,
         duration: 0.3,
       })
     } else if (
       direction === 'left' &&
       isToggle &&
-      this.startX !== this.target.offsetWidth - step
+      currentSlide !== slidersLength - 1
     ) {
+      currentSliderUpdater(currentSlide + 1)
       gsap.to(this.target, {
-        x: this.startX - step,
-        duration: 0.3,
-      })
-
-      bannerControllsCircles[currentSlide].classList.remove(
-        'banner__controll-item--active'
-      )
-      bannerControllsCircles[currentSlide + 1].classList.add(
-        'banner__controll-item--active'
-      )
-      gsap.from(bannerControllsCircles[currentSlide + 1], {
-        scale: 1.4,
+        x: -currentSlide * step,
         duration: 0.3,
       })
     } else {
