aff65c313712d223e68f8c5ed852ef195dbe6a63
Andrew <uytov1991@gmail.com>
2021-04-28 15:54:29 +0300

    исправить ползунок  в range

--- src/js/catalog-section/index.js
+++ src/js/catalog-section/index.js
@@ -46,3 +46,52 @@ const subFilterListListener = (evt) => {
 }
 
 subFilterList.addEventListener('pointerdown', subFilterListListener)
+
+// ползунок в range
+
+const rangeInputsWrapper = document.querySelector('.range-inputs-wrapper')
+
+const minInput = rangeInputsWrapper.querySelector(
+  '.filter-section__price-input--min'
+)
+const maxInput = rangeInputsWrapper.querySelector(
+  '.filter-section__price-input--max'
+)
+const rangeWrapper = rangeInputsWrapper.querySelector('.range__wrapper')
+
+rangeWrapper.addEventListener('pointerdown', (evt) => {
+  let toggle = evt.target.closest('.range__toggle')
+  if (!toggle) return
+  toggle.ondragstart = () => false
+  toggle.style.touchAction = 'none'
+  toggle.setPointerCapture(evt.pointerId)
+
+  let mutableProperty
+  toggle.classList.contains('range__toggle--left')
+    ? (mutableProperty = '--min-position')
+    : (mutableProperty = '--max-position')
+
+  let shiftX = evt.clientX - toggle.getBoundingClientRect().left
+  let newPosition
+
+  const moveAt = (evt) => {
+    newPosition = evt.pageX - shiftX - rangeWrapper.getBoundingClientRect().left
+    if (newPosition < 0) newPosition = 0
+    if (newPosition > rangeInputsWrapper.getBoundingClientRect().width) {
+      newPosition = rangeInputsWrapper.getBoundingClientRect().right
+    }
+
+    rangeWrapper.style.setProperty(mutableProperty, `${newPosition}px`)
+  }
+
+  toggle.addEventListener('pointermove', moveAt)
+
+  const mouseUp = (evt) => {
+    let step = 100 / (rangeWrapper.getBoundingClientRect().width - 48)
+
+    toggle.removeEventListener('pointermove', moveAt)
+    toggle.removeEventListener('pointerup', mouseUp)
+  }
+
+  toggle.addEventListener('pointerup', mouseUp)
+})
